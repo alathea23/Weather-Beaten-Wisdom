2259bdd00d666b93b581c924c7af4948a27d790e
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-08-26 22:44:27 -0700

    putting in logic for past searches to local storage and adjusting screen layout

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ var searchCountry = document.querySelector('#searchCountry')
 var displayResults = document.querySelector('.searchResults')
 var forecast = document.querySelectorAll('.forecast')
 var resultName = document.querySelector('#resultName')
-var resultDate = document.querySelector('#resultDate')
-var resultTemp = document.querySelector('#resultTemp')
-var resultHumidity = document.querySelector('#resultHumidity')
-var resultWind = document.querySelector('#resultWind')
+var resultDate = document.querySelectorAll('#resultDate')
+var resultIcon = document.querySelectorAll(".currentIcon")
+var resultTemp = document.querySelectorAll('#resultTemp')
+var resultHumidity = document.querySelectorAll('#resultHumidity')
+var resultWind = document.querySelectorAll('#resultWind')
 var pastSearch = document.querySelector('#past')
+var searchData = []
 var lat = '';
 var lon = '';
 var city = '';
@@ -30,7 +32,7 @@ var countryCode = '';
 var searchBtn = document.querySelector('#searchbutton');
 const apiKey = "c5fdaf1d4bca79e9226907e1b4e3b623"
 const searchDisplay = document.querySelector(".searchResults")
-const currentIcon = document.querySelector(".currentIcon")
+
 
 console.log("variables established")
 
@@ -44,6 +46,97 @@ console.log("variables established")
 //}
 
 //URLTEST()
+
+//rendering list of past search cities as clickable buttons
+function renderPastSearch() {
+    pastSearch.innerHTML = "";
+
+    for (var i = 0; i < pastSearch.length; i++) {
+        var pastCity = pastSearch[i];
+
+        var li = document.createElement("li");
+        li.setAttribute("data-index", i);
+
+        var button = document.createElement("button");
+        button.textContent = pastCity;
+
+        li.appendChild(button);
+        todoList.appendChild(li);
+    }
+}
+
+//pulling past searches from local storage
+function init() {
+    var storedSearches = JSON.parse(localStorage.getItem("pastsearches"));
+    if (storedSearches !== null) {
+        pastSearch = storedSearches;
+    }
+    renderPastSearch();
+}
+
+//adding search city info t local storage
+function storeSearches() {
+    localStorage.setItem("pastsearches", JSON.stringify(pastSearch));
+}
+
+//creating search logic to render weather info when past search city clicked
+pastSearch.addEventListener("click", function (event) {
+    var element = event.target;
+    if (element.matches("button") === true) {
+        var index = element.parentElement.getAttribute("data-index");
+        todos.splice(index, 1);
+        // TODO: What will happen when the following functions are called?
+
+        storeTodos();
+        renderTodos();
+
+        var apiURL = ('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + "&units=imperial")
+
+        fetch(apiURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                //writing weather results to display on the page
+                searchDisplay.classList.remove("hide")
+                console.log(data)
+                resultName.innerHTML = city
+                let j = 0
+                resultDate.forEach(date => {
+                    GMTDate = data.list[j].dt_txt
+                    date.innerHTML = dayjs(GMTDate).format("MMM D")
+                    j = (j + 8)
+                    console.log(j)
+                })
+                let k = 0
+                resultIcon.forEach(image => {
+                    var icon = data.list[k].weather[0].icon
+                    //source javascript image.src code: https://softauthor.com/javascript-working-with-images/
+                    image.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+                    k = (k + 8)
+                })
+                let l = 0
+                resultTemp.forEach(temp => {
+                    temp.innerHTML = "Temperature: " + data.list[l].main.temp + "º F"
+                    l = (l + 8)
+                })
+                let m = 0
+                resultHumidity.forEach(humidity => {
+                    humidity.innerHTML = "Humidity: " + data.list[m].main.humidity + "%"
+                    m = (m + 8)
+                })
+                let o = 0
+                resultWind.forEach(wind => {
+                    wind.innerHTML = "Wind Speed: " + data.list[o].wind.speed + "mph"
+                    o = (o + 8)
+                })
+            }
+            )
+    }
+});
+
+init();
+
 
 //function addWeatherToPage(respData)
 
@@ -59,6 +152,7 @@ searchBtn.addEventListener("click", function search(event) {
     var geoapiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + countryCode + "&limit=1&appid=" + apiKey
 
     console.log(geoapiURL)
+
     //setting up first API call to translate to lat/lon coordinates
     fetch(geoapiURL)
         .then(function (response) {
@@ -67,29 +161,69 @@ searchBtn.addEventListener("click", function search(event) {
         .then(function (data) {
             console.log(data);
             //capturing coordinates
-            let lat = data[0].lat
-            let lon = data[0].lon
-            var apiURL = ('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey)
+            if (data !== undefined) {
+                let lat = data[0].lat
+                let lon = data[0].lon
+                var apiURL = ('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + "&units=imperial")
 
-            console.log(apiURL)
-            //setting up second API call to get weather
-            fetch(apiURL)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    //writing weather results to display on the page
-                    searchDisplay.classList.remove("hide")
-                    console.log(data);
-                    let icon = data.list[0].weather[0].icon
-                    //source javascript image.src code: https://softauthor.com/javascript-working-with-images/
-                    currentIcon.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-                    resultName.innerHTML = city
-                    resultDate.innerHTML = data.list[0].dt_txt
-                    resultTemp.innerHTML = "Temperature: " + data.list[0].main.temp
-                    resultHumidity.innerHTML = "Humidity: " + data.list[0].main.humidity
-                    resultWind.innerHTML = "Wind Speed: " + data.list[0].wind.speed
-                });
+                console.log(apiURL)
+
+                //storing search info and building list
+                var newSearch = [{ city, lat, lon }]
+                searchData.push(newSearch)
+                searchCity.value = "";
+                searchCountry.value = "";
+                searchState.value = "";
+
+                storeSearches();
+                renderPastSearch();
+
+                //setting up second API call to get weather
+                fetch(apiURL)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        //writing weather results to display on the page
+                        searchDisplay.classList.remove("hide")
+                        console.log(data)
+                        resultName.innerHTML = city
+                        let j = 0
+                        resultDate.forEach(date => {
+                            GMTDate = data.list[j].dt_txt
+                            date.innerHTML = dayjs(GMTDate).format("MMM D")
+                            j = (j + 8)
+                            console.log(j)
+                        })
+                        let k = 0
+                        resultIcon.forEach(image => {
+                            var icon = data.list[k].weather[0].icon
+                            //source javascript image.src code: https://softauthor.com/javascript-working-with-images/
+                            image.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+                            k = (k + 8)
+                        })
+                        let l = 0
+                        resultTemp.forEach(temp => {
+                            temp.innerHTML = "Temperature: " + data.list[l].main.temp + "º F"
+                            l = (l + 8)
+                        })
+                        let m = 0
+                        resultHumidity.forEach(humidity => {
+                            humidity.innerHTML = "Humidity: " + data.list[m].main.humidity + "%"
+                            m = (m + 8)
+                        })
+                        let o = 0
+                        resultWind.forEach(wind => {
+                            wind.innerHTML = "Wind Speed: " + data.list[o].wind.speed + "mph"
+                            o = (o + 8)
+                        })
+                    }
+                    )
+            }
+            else {
+                alert("Your search parameters are not valid.  Please try again.")
+            }
         });
+});
 
-})
+
